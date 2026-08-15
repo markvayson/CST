a0e5d868243dcd7494f33cf0e1896c6bfe47c7d6
Mark <markvayson@gmail.com>
2026-08-15 20:47:45 +0400

    build(version): update application version format vx.x

--- Comtech Tools/version.h
+++ Comtech Tools/version.h
@@ -4,17 +4,16 @@
 extern std::string g_appProductName;
 extern std::string g_appVersion;
 
-
-// 1. Define your version numbers here. Update ONLY these four values.
+// 1. Define your version numbers here
 #define CSC_VERSION_MAJOR 4
 #define CSC_VERSION_MINOR 5
-#define CSC_VERSION_BUILD 0
-#define CSC_VERSION_REVISION 0
 
 // 2. Helper macros to convert the numeric values into a string
 #define STRINGIZE_NX(A) #A
 #define STRINGIZE(A) STRINGIZE_NX(A)
-#define MAKE_VERSION_STRING(a, b, c, d) STRINGIZE(a) "." STRINGIZE(b) "." STRINGIZE(c) "." STRINGIZE(d)
 
-#define CSC_VERSION_STRING MAKE_VERSION_STRING(CSC_VERSION_MAJOR, CSC_VERSION_MINOR, CSC_VERSION_BUILD, CSC_VERSION_REVISION)
+#define MAKE_VERSION_STRING(a, b) STRINGIZE(a) "." STRINGIZE(b)
+
+#define CSC_VERSION_STRING MAKE_VERSION_STRING(CSC_VERSION_MAJOR, CSC_VERSION_MINOR)
+
 
